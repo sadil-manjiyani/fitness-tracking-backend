98c69533df5e4486b9e4d4f3ef9abbb88c21ba82
sadil-08 <sadil.enpointe@gmail.com>
2024-08-17 02:32:56 +0530

    updated authentication

--- services/modules/authentication/authentication_helper.js
+++ services/modules/authentication/authentication_helper.js
@@ -1,6 +1,20 @@
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 export async function hashPassword(password) {
     const saltRounds = 10; 
     const hashedPassword = await bcrypt.hash(password, saltRounds);
     return hashedPassword;
   }
+
+  export function decodeJWT(token) {
+    try {
+      console.log("Token is ", token);
+      const decoded = jwt.decode(token, { complete: true });
+      console.log('Decoded Token:', decoded);
+      return {"status":true,"data":decoded};
+    } catch (err) {
+      console.error('Error decoding token:', err);
+      return {"status":false,"data":null};
+      
+    }
+  }  
--- services/modules/users/user_controller.js
+++ services/modules/users/user_controller.js
@@ -1,14 +1,18 @@
 import { sendResponse } from '../../../helpers/response_helper.js';
+import { decodeJWT } from '../authentication/authentication_helper.js';
 import * as userHelper from './user_helper.js';
 
 export const getUserDetails = async (req,res,next)=>{
     try{
-        let auth_token = req.headers['Authorization'];
-        let user_id;
-        //temporary
-        if(!auth_token){
-            user_id = req.params.user_id
-        } 
+        let auth_token = req.headers.authorization;
+        console.log("AUTH FROM headers", auth_token);
+        let decodedResponse = decodeJWT(auth_token);
+        if(!decodedResponse.status){
+            return sendResponse(res,[],"Cannot Get user Details",404);
+        }
+        let user_id = decodedResponse.data.payload.user_id;
+        console.log("user Id is>>>>", user_id);
+    
 
         let [user_details] = await req.app.knexConnection('u_user').where({"user_id": user_id});
 
